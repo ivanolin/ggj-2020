27fab54cbd85134d50e06d97ba127c26c0a99317
Tony Nguyen <blue28275@yahoo.com>
2020-02-01 14:24:41 -0600

    adding managers functionality for menu

--- Assets/Scripts/Managers.cs
+++ Assets/Scripts/Managers.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // a singleton component that provides global access to the game's managers (eg scene manager, audio manager, etc)
 public class Managers : MonoBehaviour
@@ -8,23 +7,33 @@ public class Managers : MonoBehaviour
     static Managers instance;
 
     private AudioManager audioManager;
-    public static AudioManager AudioManager { get { return instance?.audioManager; }}
-    
+    public static AudioManager AudioManager { get { return instance?.audioManager; } }
+
     // add more managers here
 
-    
-
-    private void Awake() {
+    private void Awake()
+    {
         // very basic singleton pattern
-        
-        if (instance == null) {
+
+        if (instance == null)
+        {
             instance = this;
-        } else if (instance != this) {
+        }
+        else if (instance != this)
+        {
             Destroy(gameObject);
         }
 
         DontDestroyOnLoad(gameObject);
     }
 
-    
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
