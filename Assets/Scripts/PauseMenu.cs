d270fd69cffc92bfc965e6f49409677c66419aaa
Basilio Bazan <basilio@cs.utexas.edu>
2020-02-02 00:37:36 -0600

    PauseWorks

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -10,7 +10,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown("joystick button 9"))
         {
             if (GameIsPaused)
             {
