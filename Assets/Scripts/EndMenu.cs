5d667692ae0fe742f05f86b0d13eda2e714b8c05
ara2798 <angel.argandonavite@gmail.com>
2020-02-02 11:39:07 -0600

    added end scene menu

---
+++ Assets/Scripts/EndMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMenu : MonoBehaviour
+{
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
